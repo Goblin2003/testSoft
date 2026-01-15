f0cfd52f9eee0d86ec45294120468c3779e49109
MoreNip <Filip-lax2016@yandex.ru>
2026-01-15 18:44:06 +0300

    Comm

---
+++ serverStatic/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace serverStatic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread writer = new Thread(() => Server.AddToCount(10));
+            Thread reader1 = new Thread(() => Console.WriteLine("Читатель1:" + Server.GetCount()));
+            Thread reader2 = new Thread(() => Console.WriteLine("Читатель2:" + Server.GetCount()));
+            writer.Start();
+            reader1.Start();
+            reader2.Start();
+        }
+    }
+    public static class Server
+    {
+        private static int count = 0;
+        private static ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+        public static int GetCount()
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                return count;
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        }
+        public static void AddToCount(int value)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                count += value;
+                Console.WriteLine($"Писатель добавил {value}");
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
+
+    }
+}
+
---
+++ test/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string stroke = "";
+            if (String.IsNullOrEmpty(stroke))
+            {
+                Console.WriteLine("Строка пустая введите строку");
+                stroke = Console.ReadLine();
+                Console.WriteLine($"Исходная строка: {stroke}");
+                Console.WriteLine($"Сжатая строка: {Compress(stroke)}"); 
+                Console.WriteLine($"Строка после декомпресии: {deCompress(Compress(stroke))}");            
+            }
+            else
+            {
+                Console.WriteLine($"Исходная строка: {stroke}");
+                Console.WriteLine($"Сжатая строка: {Compress(stroke)}");
+                Console.WriteLine($"Строка после декомпресии: {deCompress(Compress(stroke))}");
+            }
+            
+
+
+            Console.ReadLine();
+        }
+        static string Compress(string stroke)
+        {
+            
+            string result = "";
+            int count = 1;
+            for (int i = 0; i < stroke.Length; i++)
+            {
+                if (i+1 < stroke.Length && stroke[i] == stroke[i + 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    result = result + stroke[i];
+                    if (count > 1)
+                    {
+                        result = result + count;
+                    }
+                    count = 1;
+                }
+            }
+            return result;
+        }
+        static string deCompress(string stroke)  // Метод Декомпресии
+        {
+            
+            string result = "";
+            for (int i = 0; i < stroke.Length; i++)
+            {
+                char letter = stroke[i];
+                string numberStr = "";
+                while (i + 1 < stroke.Length && char.IsDigit(stroke[i + 1]))
+                {
+                    numberStr = numberStr + stroke[i + 1];
+                    i++;
+                }
+                if (numberStr == "")
+                {
+                    result = result + letter;
+
+                }
+                else
+                {
+                    int count = int.Parse(numberStr);
+                    for (int j = 0; j < count; j++)
+                    {
+                        result = result + letter;
+                    }
+                }   
+            }
+            return result;
+        }
+    }
+}
